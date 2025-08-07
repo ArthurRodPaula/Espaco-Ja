9f389a5868f0a25a4082d5377a715703e30838ca
Pedro Coelho <pedrocoelho221@gmail.com>
2025-08-07 09:17:35 -0300

    tela de locais do locador

---
+++ lib/screens/meus_locais.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class MeusLocaisScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meu locais'),
+        leading: BackButton(), // botão voltar automático
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Item 1
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    'assets/images/cowork.jpg', 
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    'co-working centro de BH',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            // Item 2
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    'assets/salao_eventos.jpg', // imagem na pasta assets
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(width: 12),
+                Expanded(
+                  child: Text(
+                    'salão de eventos em Guitierrez',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
